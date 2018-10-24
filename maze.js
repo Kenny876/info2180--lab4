1e403f1f6ada324f9fb49b38dc97502337a05998
Kennard <elliottkennard@gmail.com>
2018-10-23 23:45:27 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -12,3 +12,20 @@ window.onload = function(){
   })
   
 };
+
+function myFunction() {
+  var x = document.querySelectorAll("#maze div.boundary");
+  var end = document.getElementById("end");
+  var start = document.getElementById("start");
+  var maze = document.getElementById("maze");
+
+  for (var i = 0; i < x.length; i++) {
+    x[i].addEventListener("mouseover", colour);
+  }
+  start.onclick = function() {
+
+      var x = document.querySelectorAll("#maze div.boundary");
+      for (var i = 0; i < x.length; i++) {
+    
+      x[i].classList.remove("youlose");
+    }
